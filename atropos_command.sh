b8a62f267a275fb65c376682571d6e856fa8f205
Arman Eshaghi <arman.eshaghi@gmail.com>
2015-11-24 17:31:55 +0000

    Update atropos_command.sh

--- atropos_command.sh
+++ atropos_command.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-   Atropos -d 2 -a template.nii.gz \
-   -i PriorProbabilityImages[4,prior0%d_smoothed.nii.gz,0.25] \
-   -c [3,0] -o [output_segk.nii.gz,output_prob%0d.nii.gz] \
-   -m [0.1,1x1] \
-   -x brain_mask.nii.gz
+Atropos -d 2 -a template.nii.gz \
+-i PriorProbabilityImages[4,prior0%d_smoothed.nii.gz,0.25] \
+-c [3,0] -o [output_segk.nii.gz,output_prob%0d.nii.gz] \
+-m [0.1,1x1] \
+-x brain_mask.nii.gz
    
    
    
